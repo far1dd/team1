6fa71c40f9fa7b85983d10a5def82efb90c73e2b
Farid Huseynzada <faridhuseynzada261@gmail.com>
2023-10-23 21:44:39 +0400

    t

---
+++ home/index.js
@@ -0,0 +1,11 @@
+var btn = document.querySelector('.main-1 .left button')
+
+var ctlg = document.querySelector('.cataloglar')
+
+btn.addEventListener('click', () => {
+    window.location.href = './catalog.html'
+})
+
+ctlg.addEventListener('click', () => {
+    window.location.href = './catalog.html'
+})
