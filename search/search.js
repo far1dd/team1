67de985836d65ae2d95281f74aa68da1ac6559c7
Narmin Muradzade <murdzadenermin2@gmail.com>
2023-10-24 15:11:50 +0400

    Edited nav-bar menu size

---
+++ search/search.js
@@ -0,0 +1,6 @@
+const inp = document.querySelector('#search-book');
+const searchBtn = document.querySelector('#search-btn');
+const form = document.querySelector('#inp-form');
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+});
