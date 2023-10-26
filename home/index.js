600f8ef2be6c43e360bcdd84dcc4d0f3921a6de6
Farid Huseynzada <faridhuseynzada261@gmail.com>
2023-10-26 19:18:20 +0400

    ar

--- home/index.js
+++ home/index.js
@@ -1,6 +1,11 @@
 var btn = document.querySelector('.main-1 .left button')
-
+var joinus = document.querySelector('.joinus-logo')
 var ctlg = document.querySelector('.cataloglar')
+var popup = document.querySelector('.pop-up')
+var popupback = document.querySelector('.pop-up-back')
+
+var head = document.querySelector('header')
+
 
 btn.addEventListener('click', () => {
     window.location.href = './catalog.html'
@@ -9,3 +14,14 @@ btn.addEventListener('click', () => {
 ctlg.addEventListener('click', () => {
     window.location.href = './catalog.html'
 })
+
+joinus.addEventListener('click', () => {
+    popup.style.display = 'block'
+    popupback.style.display = 'block'
+    
+})
+
+
+
+
+
