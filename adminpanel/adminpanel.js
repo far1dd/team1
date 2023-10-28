52bcdc0201dc55300aabc620ad69a4c9affa5b15
Narmin Muradzade <murdzadenermin2@gmail.com>
2023-10-28 15:04:51 +0400

    last responsive change

---
+++ adminpanel/adminpanel.js
@@ -0,0 +1,9 @@
+const input=document.querySelector(".search .buttonandinput .search-text-input");
+const serach_button=document.querySelector('.search .buttonandinput .button_search');
+const divsearch=document.querySelector('.divsearch');
+serach_button.addEventListener('click',function(){
+   for (let i of author){
+    const li_element=document.createElement(li);
+    li_element.innerHTML
+   }
+})
