08f7d8b67d86cd1a9848bff2abe3c76251aac979
Nigar Adil <nigaradil@Nigars-MacBook-Air.local>
2023-10-31 17:51:32 +0400

    about book

--- aboutBook/aboutBook.js
+++ aboutBook/aboutBook.js
@@ -30,3 +30,36 @@ hamburger.addEventListener("click", () => {
         mainLeft.style.zIndex = "-1";
     }
 });
+
+// import { ref, set, onValue } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-database.js";
+// import { db } from './firebase.js';
+// const booksRef = ref(db, `/book`);
+// const booksList = document.querySelector(".bookForm-cont")
+// onValue(booksRef, (snapshot) => {
+//     booksList.innerHTML = ""; 
+
+//     const bookData = snapshot.val();
+//     const bookName = bookData.bookName;
+//     const authorName = bookData.authorName;
+//     const imgUrl = bookData.imgUrl;
+//     const description = bookData.description;
+//     const bookType = bookData.bookType;
+
+
+//     booksList.innerHTML = `
+//         <div>
+//         <h2 class="Basliq">${bookName}</h2>
+//         <p>${authorName}
+//         </p>
+//     </div>
+//      <div>
+//         <img src="${imgUrl}" alt="">
+//     </div>
+//     <div><p>${description}
+    
+//     </p></div>
+//     <div><p>${bookType}
+
+//             `;
+
+// });
